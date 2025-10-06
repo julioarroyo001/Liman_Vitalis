3359b17bf35380a541d8774ce219a75cf02954a6
julioarroyo001 <70067655+julioarroyo001@users.noreply.github.com>
2025-10-06 00:00:34 -0500

    Add deployment config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/urbansense-ai/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
